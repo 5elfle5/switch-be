19301b44cbd3ec11aeac4198c1e0847ae82b7cb6
Andrii Kapinos <broxbeats@gmail.com>
2020-12-04 16:12:55 +0200

    switch-core is now web project

---
+++ switch-core/src/handlers/mod.rs
@@ -0,0 +1,29 @@
+// use std::io::{ stdin, Stdin };
+// use crate::blades::BladesError;
+use actix_web::{HttpResponse};
+// use futures::StreamExt;
+
+// pub fn converse(stdin: &mut Stdin) -> Result<(), BladesError> {
+//     let mut buffer = String::new();
+//     stdin.read_line(&mut buffer)?;
+//     let answer = select_answer(buffer.trim_end());
+//     println!("{}", answer.to_string());
+//     if answer.trim_end().eq("blades!") {
+//         return Err(BladesError::new("blades!"));
+//     }
+//     Ok(())
+// }
+
+// pub fn select_answer(input: &str) -> String {
+//     if input.contains("?") { return "poppin'".to_string(); }
+//     match input {
+//         "bye!" => "blades!".to_string(),
+//         _ => "shmoopie doop".to_string()
+//     }
+// }
+
+pub fn index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/form.html"))
+}
--- switch-core/src/main.rs
+++ switch-core/src/main.rs
@@ -1,36 +1,57 @@
-use std::io::{ stdin, Stdin };
-use crate::blades::BladesError;
-
+use actix_web::{middleware, App, HttpServer};
+use actix_web::web::{resource, get};
+mod handlers;
 mod blades;
 
-fn main() -> Result<(), blades::BladesError> {
-    let mut input_channel = stdin();
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::fs::create_dir_all("./tmp").unwrap();
+    std::fs::create_dir_all("./tmp/preview").unwrap();
 
-    println!("switch switch");
-    loop {
-        match converse(&mut input_channel) {
-            Err(blades::BladesError{ details: "blades!" }) => break,
-            _ => continue
-        }
-    }
-    Ok(())
+    HttpServer::new(|| {
+        App::new().wrap(middleware::Logger::default()).service(
+            resource("/")
+                .route(get().to(handlers::index))
+        )
+    })
+    .bind("0.0.0.0:3000")?
+    .run()
+    .await
 }
 
-fn converse(stdin: &mut Stdin) -> Result<(), blades::BladesError> {
-    let mut buffer = String::new();
-    stdin.read_line(&mut buffer)?;
-    let answer = select_answer(buffer.trim_end());
-    println!("{}", answer.to_string());
-    if answer.trim_end().eq("blades!") {
-        return Err(BladesError::new("blades!"));
-    }
-    Ok(())
-}
+// use std::io::{ stdin, Stdin };
+// use crate::blades::BladesError;
 
-fn select_answer(input: &str) -> String {
-    if input.contains("?") { return "poppin'".to_string(); }
-    match input {
-        "bye!" => "blades!".to_string(),
-        _ => "shmoopie doop".to_string()
-    }
-}
+// mod blades;
+
+// fn main() -> Result<(), blades::BladesError> {
+//     let mut input_channel = stdin();
+
+//     println!("switch switch");
+//     loop {
+//         match converse(&mut input_channel) {
+//             Err(blades::BladesError{ details: "blades!" }) => break,
+//             _ => continue
+//         }
+//     }
+//     Ok(())
+// }
+
+// fn converse(stdin: &mut Stdin) -> Result<(), blades::BladesError> {
+//     let mut buffer = String::new();
+//     stdin.read_line(&mut buffer)?;
+//     let answer = select_answer(buffer.trim_end());
+//     println!("{}", answer.to_string());
+//     if answer.trim_end().eq("blades!") {
+//         return Err(BladesError::new("blades!"));
+//     }
+//     Ok(())
+// }
+
+// fn select_answer(input: &str) -> String {
+//     if input.contains("?") { return "poppin'".to_string(); }
+//     match input {
+//         "bye!" => "blades!".to_string(),
+//         _ => "shmoopie doop".to_string()
+//     }
+// }
